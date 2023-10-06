40754674bfa131a6c6a2f308f8af38dec5f8ec55
Huihan <hh2720@nyu.edu>
2023-10-06 11:25:15 -0400

    What did I get last night

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    //public Animator animator;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        //animator.SetBool("IsOpen", true);
+
+        Debug.Log("Starting conversation with" + dialogue.name);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        //animator.SetBool("IsOpen", true);
+        Debug.Log("End of consersation.");
+    }
+}
